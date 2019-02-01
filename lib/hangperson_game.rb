2f669fb8948cb125b3d00c99ce3930363cd625d0
kbarry91 <g00339811@gmit.ie>
2019-02-01 14:01:25 +0000

    Implemented guess def

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -14,6 +14,8 @@ class HangpersonGame
     @wrong_guesses = ''
   end
 
+  
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
@@ -21,6 +23,7 @@ class HangpersonGame
   attr_accessor :word
   attr_accessor :guesses
   attr_accessor :wrong_guesses
+  attr_accessor :guess
 
   def self.get_random_word
     require 'uri'
@@ -30,6 +33,39 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-  
+   # When a user makes a guess
+  def guess(letter)
+    #throw 'Error: Nil guess not allowed' if letter.nil?
+    #throw 'Error: Empty guess' if letter == ''
+    #throw 'Error: Guess not a letter' if !letter.match(/[a-zA-Z]/)
+    raise ArgumentError if letter.nil?
+    raise ArgumentError if letter == ''
+    raise ArgumentError if !letter.match(/[a-zA-Z]/)
+
+    
+    letter.downcase!
+    
+    if word.include? letter
+      unless guesses.include? letter
+        guesses << letter
+        for i in 0..word.length
+          if word[i] == letter
+            word_with_guesses[i] = letter
+            @check_win_or_lose = :win if !word_with_guesses.include? '-'
+          end
+        end
+        return true
+      end
+    else
+      unless wrong_guesses.include? letter
+        wrong_guesses << letter
+        if wrong_guesses.size >= 7
+          @check_win_or_lose = :lose
+        end
+        return true
+      end
+    end
+    return false
+  end
   
 end
